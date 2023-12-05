ec65f1f43d5b0c045d0b831b7c5439e906d76b3c
Walison Gomes <wgwalisongomes@gmail.com>
2023-12-05 10:18:23 -0300

    fix(frondend): correção de título e favicon

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,15 +1,29 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    '@nuxt/ui',
-    "@nuxtjs/tailwindcss",
-    "@nuxt/image",
-    "@pinia/nuxt",
-    "@formkit/auto-animate"
-  ],
-  ui: {
-    global: true,
-    icons: ['mdi', 'simple-icons', "heroicons"]
-  }
+    devtools: {enabled: true},
+    app: {
+        head: {
+            title: 'Cupcake Store',
+            meta: [
+                {charset: 'utf-8'},
+            ],
+            link: [
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    href: '_nuxt/assets/images/logo.png'
+                }
+            ]
+        }
+    },
+    modules: [
+        '@nuxt/ui',
+        "@nuxtjs/tailwindcss",
+        "@nuxt/image",
+        "@pinia/nuxt",
+        "@formkit/auto-animate"
+    ],
+    ui: {
+        global: true,
+        icons: ['mdi', 'simple-icons', "heroicons"]
+    }
 })
