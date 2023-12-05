9b102d277d922d70bbfeeee0e85deb79f32f35da
Walison Gomes <wgwalisongomes@gmail.com>
2023-12-04 21:24:14 -0300

    feat(frontend): configuração e instalação de dependências

---
+++ frontend/components/Logo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col items-center">
+    <img class="w-[30%]" :src="imageLoge" alt="Cupcake"/>
+    <h2 class="font-bold">Cupcake Store</h2>
+  </div>
+</template>
+<script setup lang="ts">
+import imageLoge from '@/assets/images/logo.png'
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+
+  <div class="relative h-screen">
+    <div class="absolute inset-0 bg-gradient-to-b from-white to-transparent"></div>
+    <div class="bg-cover bg-center h-screen opacity-30" :style="{
+    'background-image': `url(${restaurantImageBg})`,
+  }">
+
+    </div>
+    <div class="absolute top-0 right-0 left-0">
+      <HeaderSection></HeaderSection>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import restaurantImageBg from '@/assets/images/bg/restaurant-bg.jpeg'
+</script>
