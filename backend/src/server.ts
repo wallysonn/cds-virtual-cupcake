fb501913a332f1439774ebd6ca67019cb3a35238
Walison Gomes <wgwalisongomes@gmail.com>
2023-12-05 23:38:05 -0300

    chore(backend): ajustes para produção

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,22 +1,3 @@
-// import express from 'express';
-// import bodyParser from "body-parser";
-// import db from './db';
-//
-//
-// //import routes
-// import apiRoutes from './routes/api';
-//
-// const app = express();
-// const port: number = 4000;
-//
-// app.use('/api', apiRoutes);
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
-//
-// app.listen(port, () => {
-//     console.log(`Servidor rodando em http://localhost:${port}`);
-// });
-
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
 import {routes} from "./routes/api";
@@ -29,7 +10,7 @@ const start = async () => {
 
     try {
         await app.listen({
-            port: 4000,
+            port: (process.env.PORT || 4000) as number,
         });
     } catch (err) {
         process.exit(1)
