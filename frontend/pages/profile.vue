f7daffa7cab151c395e20d3b540667e5275c5b5f
Walison Gomes <wgwalisongomes@gmail.com>
2023-12-08 16:31:44 -0300

    feat(frontend): adicionado a foto do usuário dentro da página do perfil

--- frontend/pages/profile.vue
+++ frontend/pages/profile.vue
@@ -1,11 +1,19 @@
 <template>
   <NuxtLayout name="page">
     <div class="py-5">
-      <h1 class="text-2xl">Olá, <strong>{{store.user.name}}</strong></h1>
+      <div class="flex items-center">
+
+        <NuxtImg :src="storeUser.picture" :alt="storeUser.name"
+        class="rounded-full "
+        />
+        <h1 class="text-2xl ms-2">Olá, <strong>{{storeUser.name}}</strong></h1>
+
+      </div>
       <div class="mt-5" v-if="store.cart.length > 0">
         <h2>Conclua sua compra e aproveite o frete grátis para toda cidade</h2>
         <CartList />
       </div>
+
     </div>
   </NuxtLayout>
 </template>
@@ -13,6 +21,7 @@
 <script lang="ts" setup>
 import {appStore} from "~/stores/app.store";
 const store = appStore();
+const storeUser = store.user as UserType;
 
 
 </script>
